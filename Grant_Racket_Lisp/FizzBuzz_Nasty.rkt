7659e268f2d5985d519c34792354a8ac08004556
Grant Crofton <grantac@hotmail.com>
2014-11-27 12:55:11 +0000

    Removed 'reverse if' from nasty lisp example

--- Grant_Racket_Lisp/FizzBuzz_Nasty.rkt
+++ Grant_Racket_Lisp/FizzBuzz_Nasty.rkt
@@ -2,25 +2,24 @@
 
 ; Refuctorings Used:
 
-; Unnecessary Lambdas
-; Disguise Data
-; Inline Method
 ; Nest Deeply
-; Reverse If
+; Inline Method
+; Disguise Data
+; Unnecessary Lambdas
 
 (define fizz 
-  (lambda 
-    (number)
-    ((lambda (condition false-part true-part) (if condition true-part false-part))
+  (lambda (number)
+    ((lambda (condition true-part false-part) (if condition true-part false-part))
      ((lambda (divisor) (eq? (modulo number divisor) 0)) (char->integer #\u000F))
-     ((lambda (condition false-part true-part) (if condition true-part false-part)) 
-      ((lambda (divisor) (eq? (modulo number divisor) 0)) (char->integer #\u0005))
-      ((lambda (condition false-part true-part) (if condition true-part false-part))
-       ((lambda (divisor) (eq? (modulo number divisor) 0)) (char->integer #\u0003))(number->string number)
-       ((lambda (char-list) (apply string (map integer->char char-list))) (list 70 105 122 122)))
-      ((lambda (char-list) (apply string (map integer->char char-list))) (list 66 117 122 122)))
      ((lambda (string1 string2) (string-append string1 string2)) 
       ((lambda (char-list) (apply string (map integer->char char-list))) (list 70 105 122 122)) 
-      ((lambda (char-list) (apply string (map integer->char char-list))) (list 66 117 122 122))))))
+      ((lambda (char-list) (apply string (map integer->char char-list))) (list 66 117 122 122)))
+     ((lambda (condition true-part false-part) (if condition true-part false-part)) 
+      ((lambda (divisor) (eq? (modulo number divisor) 0)) (char->integer #\u0005))
+      ((lambda (char-list) (apply string (map integer->char char-list))) (list 66 117 122 122))
+      ((lambda (condition true-part false-part) (if condition true-part false-part))
+       ((lambda (divisor) (eq? (modulo number divisor) 0)) (char->integer #\u0003))
+       ((lambda (char-list) (apply string (map integer->char char-list))) (list 70 105 122 122))
+       ((lambda (num) (number->string num)) number))))))
   
 (provide fizz)
